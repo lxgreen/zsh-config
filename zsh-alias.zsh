ae521ff3589bca60e80c0113cd4ef3263b530ef6
lxgreen <alxgreen@gmail.com>
2019-03-31 22:25:02 +0300

    alias yarn

--- zsh-alias.zsh
+++ zsh-alias.zsh
@@ -1,17 +1,6 @@
 # photography nav
-alias acl="cd ~/photography/albums-client-lib"
-alias ass="cd ~/photography/albums-statics"
 alias org="cd ~/photography/organize-media"
 alias pro="cd ~/photography/pro-gallery-statics"
-alias wbu="cd ~/wix-base-ui"
-alias gsp="grunt serve --production"
-alias yrr="yarn pretest && ~/relink_albums.sh"
-alias rr="~/photography/rebuild_relink.sh && npm run watch"
-
-# rce nav
-alias rce="cd ~/rich-content/packages/editor"
-alias rcee="cd ~/rich-content/examples/editor"
-alias styc="cd ~/stylable-components"
 
 # hassio
 alias hass="ssh root@192.168.1.113 -i /Users/alexgr/hassio/ssh_mac/id_rsa"
@@ -34,9 +23,11 @@ alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold 
 alias gad="git ls-files --modified --others --exclude-standard | peco --initial-filter=Fuzzy | xargs git add --"
 alias gdif="git diff --name-only HEAD | peco --initial-filter=Fuzzy | xargs git diff HEAD --"
 
-# npm
+#files
 alias rpl="rm ./package-lock.json"
 alias rmnm="rm -rf ./node_modules && rm -f ./yarn.lock && rm -f ./package-lock.json"
+
+# npm
 alias nb="npm run build"
 alias nt="npm test"
 alias ns="npm start"
@@ -48,13 +39,20 @@ alias nin="npm install"
 alias nins="npm install -S"
 alias nind="npm install -D"
 alias ninf="npm install --force"
-alias and_emu="~/Library/Android/sdk/emulator/emulator"
-alias avd='(and_emu -netdelay none -netspeed full -avd Pixel_2_API_28 > $HOME/.android-avd.log &)'
-alias avdw='(and_emu -netdelay none -netspeed full -avd Pixel_2_API_28 -writable-system > $HOME/.android-avd.log &)'
-alias adb="~/Library/Android/sdk/platform-tools/adb"
-alias vm="VBoxManage -q startvm ubuntu"
 alias nbp="npm run build --prefix"
 alias nbpc="npm run build --prefix ./packages/common"
 alias nbpe="npm run build --prefix ./packages/editor"
 alias regpub="npm config set registry https://registry.npmjs.org"
 alias regwix="npm config set registry http://npm.dev.wixpress.com"
+
+#vm
+alias and_emu="~/Library/Android/sdk/emulator/emulator"
+alias avd='(and_emu -netdelay none -netspeed full -avd Pixel_2_API_28 > $HOME/.android-avd.log &)'
+alias avdw='(and_emu -netdelay none -netspeed full -avd Pixel_2_API_28 -writable-system > $HOME/.android-avd.log &)'
+alias adb="~/Library/Android/sdk/platform-tools/adb"
+alias vm="VBoxManage -q startvm ubuntu"
+
+#yarn
+alias yys="yarn && yarn start"
+alias yl="yarn link"
+alias yld="rm -rf ~/.config/yarn/link/"
