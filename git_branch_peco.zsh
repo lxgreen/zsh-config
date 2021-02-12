eaca226026fc7d0e699a06af78597c8b97e5bffb
lxgreen <alxgreen@gmail.com>
2021-02-12 20:43:06 +0200

    cleanup

--- git_branch_peco.zsh
+++
@@ -1,9 +0,0 @@
-function peco-git-checkout () {
-    local selected_branch=$(git branch --list --no-color | colrm 1 2 | peco)
-    if [ -n "$selected_branch" ]; then
-        BUFFER="git checkout ${selected_branch}"
-        zle accept-line
-    fi
-}
-zle -N peco-git-checkout
-bindkey "^bco" peco-git-checkout
